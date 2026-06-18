1ef79d46ef9d812a46b0cd5b110ebbe0cf6a3d8a
sridharakkam360 <sridharpharmkv@gmail.com>
2026-06-19 01:37:37 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <Sonner />
       <CustomCursor />
       <Chatbot />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
